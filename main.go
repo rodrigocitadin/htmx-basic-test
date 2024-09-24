44c23d93a1e8958aaf7e4634535e3731b48b29c6
rodrigocitadin <citadinrodrigo@gmail.com>
2024-09-24 13:06:27 -0300

    main server

--- main.go
+++ main.go
@@ -2,18 +2,41 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	InitDatabase()
+	defer DB.Close()
+
 	e := gin.Default()
 	e.LoadHTMLGlob("templates/*")
 
 	e.GET("/", func(ctx *gin.Context) {
+		todos := ReadToDoList()
 		ctx.HTML(http.StatusOK, "index.html", gin.H{
-			"name": "test",
+			"todos": todos,
 		})
+	})
+
+	e.POST("/todos", func(ctx *gin.Context) {
+		title := ctx.PostForm("title")
+		status := ctx.PostForm("status")
+		id, _ := CreateToDo(title, status)
+
+		ctx.HTML(http.StatusCreated, "task.html", gin.H{
+			"title":  title,
+			"status": status,
+			"id":     id,
+		})
+	})
+
+	e.DELETE("/todos/:id", func(ctx *gin.Context) {
+		param := ctx.Param("id")
+		id, _ := strconv.ParseInt(param, 10, 64)
+		DeleteTodo(id)
 	})
 
 	e.Run(":8080")
