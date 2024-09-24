db82bf0fa03b6d58ba350d47604854970a7da238
rodrigocitadin <citadinrodrigo@gmail.com>
2024-09-24 11:42:32 -0300

    wip

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    e := gin.Default()
+    e.LoadHTMLGlob("templates/*")
+
+    e.GET("/", func(ctx *gin.Context) {
+        ctx.HTML(http.StatusOK, "index.html", gin.H{
+            "name": "test",
+        })
+    })
+
+    e.Run(":8080")
+}
